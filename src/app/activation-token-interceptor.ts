3ee67529ace25f81b477729329d8007b64efe736
mihaimavrodin <mihaimavrodin9@gmail.com>
2021-12-07 13:25:32 +0200

    implemented the auth token interceptor which verifies if the jwt token has expired and the backend responds with a 403 status code of forbidden and if so, it uses the refresh token to issue another jwt token which the user will use to make further requests to the backend

--- src/app/activation-token-interceptor.ts
+++ src/app/activation-token-interceptor.ts
@@ -1,27 +1,23 @@
 import { Injectable } from "@angular/core";
 import {
-    HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
-  HttpRequest,
-  HttpResponse
+  HttpRequest
 } from "@angular/common/http";
-import { Observable, of, throwError } from "rxjs";
+import { Observable, of } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
 import { Router } from "@angular/router";
 import { AuthService } from "./auth/shared/auth.service";
 
 @Injectable()
 export class ActivationTokenInterceptor implements HttpInterceptor {
-  constructor(public router: Router, private authService: AuthService) {}
+  constructor(public router: Router) {}
 
   intercept (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     return next.handle(req).pipe(
-        tap(evt => {
-            
-        }), catchError((err:any) => {
+        tap(evt => { }), catchError((err:any) => {
             if (err.status === 404) {
                 this.router.navigate(['/404'], {skipLocationChange: true});
             } else if (err.status === 410) {
